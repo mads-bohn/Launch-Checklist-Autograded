5df6453323f0fd34437a653838f4b4ecf5003d66
mads-bohn <mady.bohn@gmail.com>
2024-03-06 20:19:13 -0600

    wrote validateInput function

--- scriptHelper.js
+++ scriptHelper.js
@@ -18,8 +18,15 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    
+    if (testInput === "") {
+        return "Empty";
+    } else if (isNaN(testInput) === true) {
+        return "Not a Number"
+    } else {
+        return "Is a Number"
+    }
  }
+
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     
