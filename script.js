7571c40b9eb035c99e154c8a4b685e9f33b984bd
mads-bohn <mady.bohn@gmail.com>
2024-03-09 15:44:29 -0600

    added empty field validation

--- script.js
+++ script.js
@@ -2,15 +2,32 @@
 
 window.addEventListener("load", function() {
 
-    let listedPlanets;
-    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    let listedPlanetsResponse;
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    })
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // });
+    
+    let form = document.querySelector("form");
+        form.addEventListener("submit", function(event) {
+            // get reference to inputs
+            let pilotInput = document.querySelector("input[name=pilotName]");
+            let copilotInput = document.querySelector("input[name=copilotName]");
+            let fuelInput = document.querySelector("input[name=fuelLevel]");
+            let massInput = document.querySelector("input[name=cargoMass]");
+
+            // alert empty input values when submitted
+            if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || massInput.value === "") {
+                alert("All fields are required!");
+                event.preventDefault();
+            }
+        });
+
+
     
  });
