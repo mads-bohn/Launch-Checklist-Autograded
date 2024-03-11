fd53c11ab60ab56fdea0a8f48eeff0e4066aafc2
mads-bohn <mady.bohn@gmail.com>
2024-03-11 03:06:40 -0500

    completed task 3 and passed test suite

--- script.js
+++ script.js
@@ -2,19 +2,28 @@
 
 window.addEventListener("load", function() {
 
-    // let listedPlanets;
-    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-    // let listedPlanetsResponse;
-    // listedPlanetsResponse.then(function (result) {
-    //     listedPlanets = result;
-    //     console.log(listedPlanets);
-    // }).then(function () {
-    //     console.log(listedPlanets);
-    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    // });
+    let listedPlanets;
+    let listedPlanetsResponse = myFetch();
+    // console.log(listedPlanetsResponse);
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        // console.log(listedPlanets);
+    }).then(function () {
+        // console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        let myPlanet = pickPlanet(listedPlanets);
+        addDestinationInfo(document, 
+            myPlanet.name, 
+            myPlanet.diameter, 
+            myPlanet.star, 
+            myPlanet.distance, 
+            myPlanet.moons, 
+            myPlanet.image);
+    });
     
     let form = document.querySelector("form");
         form.addEventListener("submit", function(event) {
+            
             // get reference to inputs
             let pilotInput = document.querySelector("input[name=pilotName]");
             let copilotInput = document.querySelector("input[name=copilotName]");
@@ -24,7 +33,8 @@ window.addEventListener("load", function() {
             // alert empty input values when submitted
             if (pilotInput.value === "" || copilotInput.value === "" || fuelInput.value === "" || massInput.value === "") {
                 alert("All fields required!");
-                event.preventDefault();
+                // event.preventDefault();
+                return;
             }
 
             // run formSubmission with correct values passed as arguments
@@ -32,8 +42,10 @@ window.addEventListener("load", function() {
                 document.getElementById("faultyItems"), 
                 pilotInput.value, 
                 copilotInput.value, 
-                document.querySelector("input[name=fuelLevel]"), 
-                document.querySelector("input[name=cargoMass]"));
+                fuelInput.value, 
+                massInput.value);
+
+            event.preventDefault();
             
         });
 
